db50ccd2e3f0f935d1d2f0eb8e387547e6a2ea07
gmo93 <omohundrogrant@yahoo.com>
2023-05-12 00:52:30 -0500

    changed <img> to Image from 'next/image'

--- src/pages/components/PhotoConst.jsx
+++ src/pages/components/PhotoConst.jsx
@@ -1,13 +1,14 @@
-import React, {Image, useState} from 'react';
+import React, {useState} from 'react';
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 export default function PhotoConst() {
-    const realEstatePhotos = ['kitchenDoor.jpeg', 'kitchenFromDoor.jpeg', 'livingRoom.jpeg'];
-    const productPhotos = ['brownChairBack.jpeg', 'brownChairFront.jpeg', 'threeChairsStraight.jpeg', 'threeChairsSideways.jpeg'];
-    const travelPhotos = ['canopy1.jpeg', 'canopy2.jpeg'];
-    const artPhotos = ['jordanPainting1.jpeg', 'jordanPainting2.jpeg', 'peter.jpeg', 'peterAndChance.jpeg']
+    const realEstatePhotos = ['/kitchenDoor.jpeg', '/kitchenFromDoor.jpeg', '/livingRoom.jpeg'];
+    const productPhotos = ['/brownChairBack.jpeg', '/brownChairFront.jpeg', '/threeChairsStraight.jpeg', '/threeChairsSideways.jpeg'];
+    const travelPhotos = ['/canopy1.jpeg', '/canopy2.jpeg'];
+    const artPhotos = ['/jordanPainting1.jpeg', '/jordanPainting2.jpeg', '/peter.jpeg', '/peterAndChance.jpeg']
     const renderNames = (names) => {
-        return names.map(name => <img alt = {name} key={name} className={styles.photo} width = {400} src = {name}/>)
+        return names.map(name => <Image alt = {`${name} here`} key={name} className={styles.photo} layout="fill" src = {name}></Image>)
     }
     // const [modalOpen, setModalOpen] = useState(false);
     // const [bigImage, setBigImage] = useState();
